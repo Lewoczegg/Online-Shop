813da129262ac6d0ecd59f3bed109b3034f5c0e5
Mateusz Lewocz <82779@student.pb.edu.pl>
2024-06-05 10:40:20 +0200

    Initialize backend

---
+++ OnlineShopBackend/src/test/java/wojtki/onlineshopbackend/OnlineShopBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package wojtki.onlineshopbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineShopBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
