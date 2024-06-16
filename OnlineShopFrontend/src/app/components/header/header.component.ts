bd26dd73963049c30f3a1a2701049e22ae1e9e08
Mateusz Lewocz <82779@student.pb.edu.pl>
2024-06-16 13:05:59 +0200

    Create profile dropdown menu

--- OnlineShopFrontend/src/app/components/header/header.component.ts
+++ OnlineShopFrontend/src/app/components/header/header.component.ts
@@ -2,17 +2,21 @@ import { Component, OnInit } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { UserService } from '../../services/user.service';
 import { User } from '../../models/user.model';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, FontAwesomeModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
 export class HeaderComponent implements OnInit {
   user: User | null = null;
   authorities: string[] | undefined = [];
+  isDropdownOpen: boolean = false;
+  faUser = faUser;
   constructor(private userService: UserService, private router: Router) {}
 
   ngOnInit(): void {
@@ -27,5 +31,9 @@ export class HeaderComponent implements OnInit {
     window.sessionStorage.setItem('userdetails', '');
     window.sessionStorage.removeItem('Authorization');
     this.router.navigate(['login']);
+  }
+
+  toggleDropdown(): void {
+    this.isDropdownOpen = !this.isDropdownOpen;
   }
 }
